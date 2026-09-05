3f8d7b2df2b3efbe77eca0d0229a3305c056007d
Clara Vas <vasconcelosousa2508@gmail.com>
2026-09-05 13:51:55 -0300

    Delete avaliacoes/avaliacao-3 directory

--- avaliacoes/avaliacao-3/README.md
+++
@@ -1,3 +0,0 @@
-# Atividade3
-
-Crie uma aplicação Dart que acessa um banco de dados com tratamento de exceção.
--- avaliacoes/avaliacao-3/test/avaliacao_3_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:avaliacao_3/avaliacao_3.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
