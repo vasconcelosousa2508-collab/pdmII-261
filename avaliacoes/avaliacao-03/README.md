5b570a8fc55e57343cf0cf70fd84179cab69844e
Clara Vas <vasconcelosousa2508@gmail.com>
2026-09-05 13:52:39 -0300

    Add files via upload

---
+++ avaliacoes/avaliacao-03/README.md
@@ -0,0 +1,3 @@
+# Atividade3
+
+Crie uma aplicação Dart que acessa um banco de dados com tratamento de exceção.
---
+++ avaliacoes/avaliacao-03/test/avaliacao_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:avaliacao_3/avaliacao_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
